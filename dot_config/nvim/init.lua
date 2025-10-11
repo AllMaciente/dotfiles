6883e0541cc07fa968e907fac0344b4e979833fc
allm <allmacdev@gmail.com>
2025-10-10 21:46:25 -0300

    nvim:config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,1 +1,19 @@
 require("config.lazy")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 5
+
+-- Formatar ao salvar (usando LSP)
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
