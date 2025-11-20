6ec534f1cdacd5cde27cca83a36ae08581a2ef43
allm <allmacdev@gmail.com>
2025-11-20 10:58:37 -0300

    Update .config/nvim/lua/plugins/theme.lua

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,31 @@
 return {
   {
     "AlphaTechnolog/pywal.nvim",
+    lazy = false, -- Importante: Garante que carregue no início
     config = function()
-      require("pywal").setup()
+      local pywal = require("pywal")
+      
+      pywal.setup()
       vim.cmd("colorscheme pywal")
+
+      -- --- MÁGICA DO LIVE RELOAD ---
+      -- Cria um "ouvinte" para o sinal SIGUSR1
+      vim.api.nvim_create_autocmd("Signal", {
+        pattern = "SIGUSR1",
+        callback = function()
+          vim.schedule(function()
+            -- 1. Recarrega as cores
+            vim.cmd("colorscheme pywal")
+            
+            -- 2. Se você usa Lualine, força a atualização dele também
+            if package.loaded['lualine'] then
+               require('lualine').refresh()
+            end
+            
+            print("Cores do Pywal atualizadas!")
+          end)
+        end,
+      })
     end,
   },
 }
