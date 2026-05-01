1802bc881bce31bd98e93447f4b74791402fb9bc
allm <allan@allmaciente.dev.br>
2026-05-01 10:47:36 -0400

    Update .config/hypr/autostart.conf Update .config/hypr/keybinds.conf Update .config/rofi/bin/rofi-hue.sh

--- dot_config/rofi/bin/executable_rofi-hue.sh
+++ dot_config/rofi/bin/executable_rofi-hue.sh
@@ -3,15 +3,11 @@
 # 1. Gera a lista de temas limpa
 theme_list=$(huectl list | grep -E '^  [○●]' | sed 's/^[[:space:]]*[○●][[:space:]]*//')
 
-# 2. Adiciona a opção "Random" no topo e abre o Rofi
-# O pipe "echo -e" serve para colocar o Random antes da lista original
-selected=$(echo -e "󰒡 Random Theme\n$theme_list" | rofi -dmenu -p "Selecionar Tema:")
+selected=$(echo -e "󰑓 reload\n$theme_list" | rofi -dmenu -p "Selecionar Tema:")
 
 # 3. Lógica de aplicação
-if [ "$selected" = "󰒡 Random Theme" ]; then
-    # Sorteia um tema da lista original
-    random_theme=$(echo "$theme_list" | shuf -n 1)
-    huectl apply "$random_theme"
+if [ "$selected" = "󰑓 reload" ]; then
+    huectl reload
 elif [ -n "$selected" ]; then
     # Aplica o tema específico selecionado
     huectl apply "$selected"
